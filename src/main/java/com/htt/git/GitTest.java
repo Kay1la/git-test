2a33212022265032d38a2113b179d4d2d827e702
Kayla <Kayla@qq.com>
2022-07-05 09:04:52 +0800

    second commit

--- src/main/java/com/htt/git/GitTest.java
+++ src/main/java/com/htt/git/GitTest.java
@@ -4,5 +4,6 @@ package com.htt.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git");
     }
 }
